c4c8998290e56fc83753b3a383a4beea8be40237
Muhammad Arsalan <arslan.gzr@gmail.com>
2024-10-09 14:21:49 +0500

    Added import of abort function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, abort
 from models import db, EmployeeModel
  
 app = Flask(__name__)
